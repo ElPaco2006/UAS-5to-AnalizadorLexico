da46d2f07b1126cac44f33ec66ad4d983b215a80
Francisco Javier <franciscoinzunza2006@gmail.com>
2026-09-02 15:16:02 -0700

    Renamed EOF to EOF (End Of Line)

--- lexer.c
+++ lexer.c
@@ -5,8 +5,10 @@
 #include <string.h>
 #include "lexer.h"
 
+#include <stdio.h>
+
 typedef enum {
-    EOF, // \0
+    EOL, // \0
 
     LETTER,   // A-Z, a-z, _
     EXPONENT, // E, e
@@ -50,12 +52,12 @@ typedef enum {
     ST_IDENTIFIER_WITH_NUMBERS = 18,
 
     STATE_COUNT,
-} State;
+} LexerState;
 
 static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
     [ST_NULL]  = {0},
     [ST_START] = {
-        [EOF]           = ER_EMPTY,
+        [EOL]           = ER_EMPTY,
         [LETTER]        = ST_IDENTIFIER,
         [EXPONENT]      = ST_IDENTIFIER,
         [DIGIT]         = ST_NUMBER,
@@ -69,7 +71,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
     },
 
     [ST_NUMBER] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_NUMBER_INVALID_CHARACTER,
         [EXPONENT]      = ST_NUMBER_EXPONENT,
         [DIGIT]         = ST_NUMBER,
@@ -82,7 +84,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_NUMBER_INVALID_CHARACTER,
     },
     [ST_NUMBER_POINT] = {
-        [EOF]           = ER_NUMBER_MISSING_DECIMAL_PART,
+        [EOL]           = ER_NUMBER_MISSING_DECIMAL_PART,
         [LETTER]        = ER_NUMBER_INVALID_CHARACTER,
         [EXPONENT]      = ER_NUMBER_MISSING_DECIMAL_PART,
         [DIGIT]         = ST_NUMBER_DECIMALS,
@@ -96,7 +98,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         //[EOF] = ST_NULL, [DIGIT] = ST_NUMBER_DECIMALS
     },
     [ST_NUMBER_DECIMALS] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_INVALID_CHARACTER,
         [EXPONENT]      = ST_NUMBER_EXPONENT,
         [DIGIT]         = ST_NUMBER_DECIMALS,
@@ -110,7 +112,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         //[EOF] = ST_NULL, [DIGIT] = ST_NUMBER_DECIMALS, [EXPONENT] = ST_NUMBER_EXPONENT
     },
     [ST_NUMBER_EXPONENT] = {
-        [EOF]           = ER_NUMBER_MISSING_EXPONENT_PART,
+        [EOL]           = ER_NUMBER_MISSING_EXPONENT_PART,
         [LETTER]        = ER_INVALID_CHARACTER,
         [EXPONENT]      = ER_NUMBER_MORE_THAN_ONE_EXPONENT,
         [DIGIT]         = ST_NUMBER_EXPONENT_AMOUNT,
@@ -124,7 +126,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         //[EOF] = ST_NULL, [DIGIT] = ST_NUMBER_EXPONENT_AMOUNT, [PLUS] = ST_NUMBER_EXPONENT_SIGN, [MINUS] = ST_NUMBER_EXPONENT_SIGN
     },
     [ST_NUMBER_EXPONENT_SIGN] = {
-        [EOF]           = ER_NUMBER_MISSING_EXPONENT_PART,
+        [EOL]           = ER_NUMBER_MISSING_EXPONENT_PART,
         [LETTER]        = ER_NUMBER_INVALID_CHARACTER,
         [EXPONENT]      = ER_NUMBER_MORE_THAN_ONE_EXPONENT,
         [DIGIT]         = ST_NUMBER_EXPONENT_AMOUNT,
@@ -137,7 +139,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_NUMBER_INVALID_CHARACTER,
     },
     [ST_NUMBER_EXPONENT_AMOUNT] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_NUMBER_INVALID_CHARACTER,
         [EXPONENT]      = ER_NUMBER_MORE_THAN_ONE_EXPONENT,
         [DIGIT]         = ST_NUMBER_EXPONENT_AMOUNT,
@@ -152,7 +154,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
     },
 
     [ST_STRING_OPEN] = {
-        [EOF]           = ER_STRING_LEFT_OPEN,
+        [EOL]           = ER_STRING_LEFT_OPEN,
         [LETTER]        = ST_STRING_OPEN,
         [EXPONENT]      = ST_STRING_OPEN,
         [DIGIT]         = ST_STRING_OPEN,
@@ -165,7 +167,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ST_STRING_OPEN,
     },
     [ST_STRING_CLOSED] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_STRING_ALREADY_CLOSED,
         [EXPONENT]      = ER_STRING_ALREADY_CLOSED,
         [DIGIT]         = ER_STRING_ALREADY_CLOSED,
@@ -179,7 +181,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
     },
 
     [ST_OPERATOR_PLUS] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_INVALID_CHARACTER,
         [EXPONENT]      = ER_OPERATOR_INVALID_CHARACTER,
         [DIGIT]         = ER_OPERATOR_INVALID_CHARACTER,
@@ -192,7 +194,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_OPERATOR_INVALID_CHARACTER,
     },
     [ST_OPERATOR_MINUS] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_INVALID_CHARACTER,
         [EXPONENT]      = ER_OPERATOR_INVALID_CHARACTER,
         [DIGIT]         = ST_NUMBER,
@@ -205,7 +207,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_OPERATOR_INVALID_CHARACTER
     },
     [ST_OPERATOR_OTHER] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_INVALID_CHARACTER,
         [EXPONENT]      = ER_OPERATOR_INVALID_CHARACTER,
         [DIGIT]         = ER_OPERATOR_INVALID_CHARACTER,
@@ -219,7 +221,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
 
     },
     [ST_OPERATOR_INCREMENT] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_TOO_LONG,
         [EXPONENT]      = ER_OPERATOR_TOO_LONG,
         [DIGIT]         = ER_OPERATOR_TOO_LONG,
@@ -233,7 +235,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
 
     },
     [ST_OPERATOR_DECREMENT] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_TOO_LONG,
         [EXPONENT]      = ER_OPERATOR_TOO_LONG,
         [DIGIT]         = ER_OPERATOR_TOO_LONG,
@@ -247,7 +249,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
 
     },
     [ST_OPERATOR_COMPLEX] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_OPERATOR_TOO_LONG,
         [EXPONENT]      = ER_OPERATOR_TOO_LONG,
         [DIGIT]         = ER_OPERATOR_TOO_LONG,
@@ -261,7 +263,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
     },
 
     [ST_IDENTIFIER] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ST_IDENTIFIER,
         [EXPONENT]      = ST_IDENTIFIER,
         [DIGIT]         = ST_IDENTIFIER_WITH_NUMBERS,
@@ -274,7 +276,7 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_IDENTIFIER_INVALID_CHARACTER,
     },
     [ST_IDENTIFIER_WITH_NUMBERS] = {
-        [EOF]           = ST_NULL,
+        [EOL]           = ST_NULL,
         [LETTER]        = ER_IDENTIFIER_LETTER_AFTER_DIGIT,
         [EXPONENT]      = ER_IDENTIFIER_LETTER_AFTER_DIGIT,
         [DIGIT]         = ST_IDENTIFIER_WITH_NUMBERS,
@@ -287,29 +289,28 @@ static int lexer_matrix[STATE_COUNT][SYMBOL_COUNT] = {
         [INVALID]       = ER_IDENTIFIER_INVALID_CHARACTER,
     },
 };
-static char* error_msg[ERROR_COUNT] = {0};
 
 static bool isDigit(char c);
 static bool isOperator(char c);
 static bool isLetter(char c);
-
 static SymbolType getSymbolType(char symbol);
+
 static bool isKeyword(const char* word);
 
 static bool isError(int state);
-static void printError(int error, const char* input);
+static void printError(int error, const char* input, char last_char);
 
 LexerResult parseLexeme(const char* input, TokenType* token_type) {
-    State state        = ST_START;
+    LexerState state   = ST_START;
     LexerResult result = LEX_SUCCESS;
 
     const char* c = input;
     while (true) {
-        const SymbolType type  = getSymbolType(*c);
-        const State next_state = lexer_matrix[state][type];
+        const SymbolType type       = getSymbolType(*c);
+        const LexerState next_state = lexer_matrix[state][type];
 
         if (isError(next_state)) {
-            printError(next_state, input);
+            printError(next_state, input, *c);
             result = (LexerResult)next_state;
             break;
         }
@@ -337,9 +338,10 @@ LexerResult parseLexeme(const char* input, TokenType* token_type) {
     return result;
 }
 
+#pragma region Get character category
 static SymbolType getSymbolType(const char symbol) {
     switch (symbol) {
-        case '\0': return EOF;
+        case '\0': return EOL;
         case '+': return PLUS;
         case '-': return MINUS;
         case '=': return EQUALS;
@@ -357,7 +359,7 @@ static SymbolType getSymbolType(const char symbol) {
     return INVALID;
 }
 
-bool isDigit(char c) {
+bool isDigit(const char c) {
     return c >= '0' && c <= '9';
 }
 
@@ -372,11 +374,12 @@ bool isOperator(const char c) {
     return false;
 }
 
-bool isLetter(char c) {
+bool isLetter(const char c) {
     return (c >= 'a' && c <= 'z') ||
         (c >= 'A' && c <= 'Z') ||
         c == '_';
 }
+#pragma endregion
 
 static bool isKeyword(const char* word) {
     static const char* keywords[] = {
@@ -425,4 +428,30 @@ static bool isError(const int state) {
     return state >= ERROR_OFFSET && state < ERROR_COUNT;
 }
 
-static void printError(int error, const char* input) {}
+static void printError(int error, const char* input, char last_char) {
+    const char* error_messages[] = {
+        [ER_IDK]                                = "",
+        [ER_EMPTY]                              = "",
+        [ER_INVALID_CHARACTER]                  = "",
+        [ER_NUMBER_MISSING_INTEGER_PART]        = "",
+        [ER_NUMBER_MISSING_DECIMAL_PART]        = "",
+        [ER_NUMBER_MISSING_EXPONENT_PART]       = "",
+        [ER_NUMBER_MORE_THAN_ONE_DECIMAL_POINT] = "",
+        [ER_NUMBER_DECIMAL_ON_EXPONENT]         = "",
+        [ER_NUMBER_MORE_THAN_ONE_EXPONENT]      = "",
+        [ER_NUMBER_MORE_THAN_ONE_EXPONENT_SIGN] = "",
+        [ER_NUMBER_INVALID_CHARACTER]           = "",
+        [ER_STRING_LEFT_OPEN]                   = "",
+        [ER_STRING_ALREADY_CLOSED]              = "",
+        [ER_OPERATOR_TOO_LONG]                  = "",
+        [ER_OPERATOR_INVALID_COMPLEX]           = "",
+        [ER_OPERATOR_INVALID_CHARACTER]         = "",
+        [ER_IDENTIFIER_INVALID_CHARACTER]       = "",
+        [ER_IDENTIFIER_LETTER_AFTER_DIGIT]      = ""
+    };
+
+    if (!isError(error)) error = ER_IDK;
+    const char* errormsg = error_messages[error - ERROR_OFFSET];
+    printf("Algo salio mal. Código de error: %d\n", error);
+    printf(errormsg, input, last_char);
+}
